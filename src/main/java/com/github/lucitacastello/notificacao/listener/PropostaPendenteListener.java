886bd8a747a6c5891a63934d821b3e51e7282d8f
cidarosa <lucita.rosa@gmail.com>
2024-06-26 16:29:11 -0300

    add NotificacaoSnsService

--- src/main/java/com/github/lucitacastello/notificacao/listener/PropostaPendenteListener.java
+++ src/main/java/com/github/lucitacastello/notificacao/listener/PropostaPendenteListener.java
@@ -2,16 +2,20 @@ package com.github.lucitacastello.notificacao.listener;
 
 import com.github.lucitacastello.notificacao.constante.MensagemConstante;
 import com.github.lucitacastello.notificacao.domain.Proposta;
+import com.github.lucitacastello.notificacao.service.NotificacaoSnsService;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
 @Component
 public class PropostaPendenteListener {
 
+    private NotificacaoSnsService notificacaoSnsService;
+
     @RabbitListener(queues = "${rabbitmq.queue.proposta.pendente}")
     public void propostaPendente(Proposta proposta){
         //msg que quer mandar via SMS
         String mensagem = String.format(MensagemConstante.PROPOSTA_EM_ANALISE, proposta.getUsuario().getNome());
+        notificacaoSnsService.notificar(mensagem);
 
     }
 
---
+++ src/main/java/com/github/lucitacastello/notificacao/service/NotificacaoSnsService.java
@@ -0,0 +1,12 @@
+package com.github.lucitacastello.notificacao.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class NotificacaoSnsService {
+
+    public void notificar(String mensagem){
+        //vai integrar com SNS
+
+    }
+}
