79ca61eb1c3547f4a5840de0393a63e4f1687232
cidarosa <lucita.rosa@gmail.com>
2024-06-26 16:25:21 -0300

    add MensagemConstante

--- src/main/java/com/github/lucitacastello/notificacao/listener/PropostaPendenteListener.java
+++ src/main/java/com/github/lucitacastello/notificacao/listener/PropostaPendenteListener.java
@@ -1,5 +1,7 @@
 package com.github.lucitacastello.notificacao.listener;
 
+import com.github.lucitacastello.notificacao.constante.MensagemConstante;
+import com.github.lucitacastello.notificacao.domain.Proposta;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
@@ -7,7 +9,9 @@ import org.springframework.stereotype.Component;
 public class PropostaPendenteListener {
 
     @RabbitListener(queues = "${rabbitmq.queue.proposta.pendente}")
-    public void propostaPendente(){
+    public void propostaPendente(Proposta proposta){
+        //msg que quer mandar via SMS
+        String mensagem = String.format(MensagemConstante.PROPOSTA_EM_ANALISE, proposta.getUsuario().getNome());
 
     }
 
