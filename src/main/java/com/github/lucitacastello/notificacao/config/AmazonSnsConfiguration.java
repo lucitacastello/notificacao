a3b17f3a57762db5f85d024748cb9db75112d5dc
cidarosa <lucita.rosa@gmail.com>
2024-06-26 17:06:39 -0300

    add Beans config para autenticar na AWS

--- src/main/java/com/github/lucitacastello/notificacao/config/AmazonSnsConfiguration.java
+++ src/main/java/com/github/lucitacastello/notificacao/config/AmazonSnsConfiguration.java
@@ -1,6 +1,13 @@
 package com.github.lucitacastello.notificacao.config;
 
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.sns.AmazonSNS;
+import com.amazonaws.services.sns.AmazonSNSClientBuilder;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
@@ -11,5 +18,22 @@ public class AmazonSnsConfiguration {
 
     @Value("${aws.secretKey}")
     private String secretKey;
+
+    //configuração dos Beans
+    @Bean
+    public AWSCredentials awsCredentials() {
+
+        return new BasicAWSCredentials(acessKey, secretKey);
+
+    }
+
+    @Bean
+    public AmazonSNS amazonSNS() {
+
+        return AmazonSNSClientBuilder.standard()
+                .withCredentials(new AWSStaticCredentialsProvider(awsCredentials()))
+                .withRegion(Regions.US_EAST_1).build();
+
+    }
 
 }
