35659ed35f60ca7ca552ba5b50646bc8e45ccb63
cidarosa <lucita.rosa@gmail.com>
2024-06-26 16:06:30 -0300

    add classes do domain. add proprieda do ex application.properties

---
+++ src/main/java/com/github/lucitacastello/notificacao/domain/Usuario.java
@@ -0,0 +1,18 @@
+package com.github.lucitacastello.notificacao.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+
+public class Usuario {
+
+    private Long id;
+    private String nome;
+    private String sobrenome;
+    private String cpf;
+    private String telefone;
+    private Double renda;
+
+}
